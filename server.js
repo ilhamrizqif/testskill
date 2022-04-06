f78a83966c36f67f998fe9f819f6734352383c88
ilhamrizqif <ir.fadillah27@gmail.com>
2022-04-06 12:32:43 +0700

    setting routes

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app=express();
+//parse application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+//calling routes
+var routes=require('./routes');
+routes(app);
+
+
 app.listen(3300, () => {
     console.log(`Server started on port`);
+
+
 });
